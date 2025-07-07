6f9e06a2aab0b1e3ce5db066aa213641a1b4a216
Saherish Kazi <saherishkazi@gmail.com>
2025-07-07 22:21:45 +0530

    Add files via upload

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Fibonacci {
+    public static int fibo(int n){
+        if(n == 0 || n==1){
+            return n;
+        }
+    
+
+        int fib0 = 0;
+        int fib1 = 1;
+        int fnm1 = fibo(n-1);
+        int fnm2 = fibo(n-2);
+        int fib = fnm1 + fnm2;
+        return fib;
+    }
+    public static void main(String args[]){
+        int n =5;
+        System.out.println(fibo(n));
+    }
+    
+}
