1340de690da1f19f78ff3e7872edceb25cacd5c3
Saherish Kazi <saherishkazi@gmail.com>
2025-07-11 22:20:13 +0530

    Create Basic.java

---
+++ ArrayList/Basic.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class basic {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+
+        //get
+        // int element =list.get(2);
+        // System.out.println(element);
+
+        //set
+        // list.set(2,10);
+        // System.out.println(list);
+        
+        //contain
+        // System.out.println(list.contains(2));
+
+        //size
+        // System.out.println(list.size());
+
+        //iterate
+        // for(int i=0; i<list.size();i++){
+        //     System.out.print(list.get(i)+" ");
+        // }
+
+//reverse the arraylist
+        // for(int i=list.size()-1; i>=0; i--){
+        //     System.out.print(list.get(i)+" ");
+        // }
+
+        //maximum value  O(n)
+
+        // int max = Integer.MIN_VALUE;
+        // for(int i=0; i< list.size();i++){
+            // if(max < list.get(i)){
+            //     max = list.get(i);
+            // }
+    //         max = Math.max(max, list.get(i));
+    //     }
+    //     System.out.print(max);
+    // }
+
+  
+  
+    
+}
+}
