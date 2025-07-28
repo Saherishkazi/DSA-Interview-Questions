0d00bbbf7953926384ea63c54e7aad412487cb37
Saherish Kazi <saherishkazi@gmail.com>
2025-07-28 20:14:54 +0530

    Create Create_LinkedList.java

---
+++ LinkedList/Create_LinkedList.java
@@ -0,0 +1,30 @@
+ public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data =data;
+            this.next =null;
+        }
+
+    }
+        public static Node head;
+        public static Node tail;
+
+        public  void addFirst(int data){
+             Node newNode = new Node(data);
+             if(head == null){
+                head=tail=newNode;
+                return;
+             }
+             newNode.next = head;
+            head = newNode;
+    }
+     public static void main(String arg[]){
+       prac ll =new prac();
+       ll.addFirst(1);
+       ll.addFirst(2);
+       ll.addFirst(3);
+       ll.print();
+    }
+}
