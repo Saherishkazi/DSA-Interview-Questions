9462d3d5b70ec311f2138d736f89eb81d431255f
Saherish Kazi <saherishkazi@gmail.com>
2025-07-30 00:12:49 +0530

    Create reverse.java

---
+++ LinkedList/reverse.java
@@ -0,0 +1,13 @@
+ public void reverse(){
+        Node prev =null;
+        Node curr =tail = head;
+        Node next;
+        
+        while(curr != null){
+            next =curr.next;
+            curr.next =prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
