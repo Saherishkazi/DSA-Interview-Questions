b1200851c04d1eb5ebf7cf746debce2235d18e6d
Saherish Kazi <saherishkazi@gmail.com>
2025-08-01 21:52:30 +0530

    Create Search_recursive.java

---
+++ LinkedList/Search_recursive.java
@@ -0,0 +1,16 @@
+ public int helper(Node head, int key){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next, key);
+        if(idx == -1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recursive(int key){
+        return helper( head ,key);
+    }
