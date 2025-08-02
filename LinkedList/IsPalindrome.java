a12a1a3fbfaf29f2d19a73edad76e799b0f95c93
Saherish Kazi <saherishkazi@gmail.com>
2025-08-02 17:43:01 +0530

    Update IsPalindrome.java

--- LinkedList/IsPalindrome.java
+++ LinkedList/IsPalindrome.java
@@ -1,32 +1,32 @@
-public boolean palindrome(int n){
-    if(head == null || head.next == null){
+ public boolean checkPlindrome(){
+        //base
+        if(head == null && head.next ==null){
+            return true;
+        }
+        //1- find mid
+        Node midNode = findmid(head);
+
+        //2-reverse 2nd hald
+        Node prev =null;
+        Node curr =midNode;
+        Node next;
+
+        while(curr != null){
+            next =curr.next;
+            curr.next = prev;
+            prev =curr;
+            curr =next;
+        }
+       Node left = head;
+       Node right = prev;
+        //3- check left and right half
+        while(right != null){
+
+            while(left.data != right.data){
+                return false;
+            }
+            left =left.next;
+            right = right.next;
+        }
         return true;
     }
-    //find mid
-    int mid =findMid(head);
-
-    //reverse
-   
-Node prev =null;
-Node curr =mid;
-Node next;
-
-while(curr != null){
-   next =curr.next;
-curr.next =prev;
-prev =curr;
-curr =next;
-}
-Node right =prev;
-Node left =head;
-
-//check
-while(right != null){
-    if(left.data != right.data){
-        return false;
-   left =left.next;
-   right =right.next;
-}
-return true;
-
-}
