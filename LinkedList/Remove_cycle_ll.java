4d82deef4a5d32f0c078ac6429003fb2c3000ed2
Saherish Kazi <saherishkazi@gmail.com>
2025-08-04 19:02:35 +0530

    Create Remove_cycle_ll.java

---
+++ LinkedList/Remove_cycle_ll.java
@@ -0,0 +1,32 @@
+ public static void removeCycle(){
+        //detect cycle
+        Node slow =head;
+        Node fast =head;
+        boolean cycle =false;
+
+        while(fast != null && fast.next !=null) {
+            slow =slow.next;
+            fast =fast.next.next;
+            if(fast == slow){
+                cycle=true;
+                break;
+            }
+        }
+        if( cycle == false){
+            return;
+        }
+
+        //find meeting point
+        slow =head;
+        Node prev =null;
+        
+        while(slow != fast){
+            prev =fast;  //last node
+            slow =slow.next;
+            fast =fast.next;
+
+        }
+        //remove cycle
+        prev.next = null;
+    }
+
