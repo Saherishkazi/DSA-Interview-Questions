357aef8b2584da4f4a49bc016d47d22a22542bf6
Saherish Kazi <saherishkazi@gmail.com>
2025-08-08 18:02:57 +0530

    Create DLL_Reverse.java

---
+++ LinkedList/DLL_Reverse.java
@@ -0,0 +1,14 @@
+   public void reverse(){
+            Node curr =head;
+            Node prev =null;
+            Node next;
+            while(curr != null){
+                next =curr.next; //
+                curr.next =prev; //
+                curr.prev =next; //
+                prev =curr;
+                curr =next;
+
+            }
+            head =prev;
+        }
